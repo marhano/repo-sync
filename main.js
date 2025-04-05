ea1503cb5b5aadc14c1c943613e77f8e8f75ec28
akocmarjan <akocmarjan@gmail.com>
2025-04-05 13:28:59 +0800

    Setup electron js

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('dist/repo-sync/browser/index.html');
+}
+
+app.on('ready', createWindow);
