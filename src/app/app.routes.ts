22ebca8ad66b657c6a29d29a9f5dcfc2603b3c11
marhano <carullomarjan@gmail.com>
2025-04-12 22:27:09 +0800

    Added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { IssueComponent } from './pages/issue/issue.component';
 
 export const routes: Routes = [
     { 
         path: 'home',
         loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
     },
-    { path: 'issue', component: IssueComponent },
+    { 
+        path: 'issue', 
+        loadComponent: () => import('./pages/issue/issue.component').then(c => c.IssueComponent)     
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(c => c.LoginComponent)
+    },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: '**', component: HomeComponent }
+    { 
+        path: '**', 
+        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
+    }
 ];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
