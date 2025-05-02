a6d31cb8a93453e913029ed8bb84c01a040bfd25
marhano <carullomarjan@gmail.com>
2025-05-02 13:49:43 +0800

    Refactor dashboard hover functionality to improve loading state and adjust hover container positioning

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { GitApiService } from '../../services/git-api/git-api.service';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MarkdownModule } from 'ngx-markdown';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,7 +15,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     CommonModule,
     MatIconModule,
     MarkdownModule,
-    MatProgressBarModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
@@ -27,6 +27,10 @@ export class DashboardComponent {
   public mouseY: number = 0;
   public readme: any;
   public hoverDirection: string = 'right'; 
+  public isLoading: boolean = true;
+  public containerWidth: number = 0;
+
+  @ViewChild('hoverContainer', { static: false }) hoverContainer!: ElementRef;
 
   constructor(
     private gitApiService: GitApiService,
@@ -47,9 +51,18 @@ export class DashboardComponent {
 
   async onMouseEnter(item: any){
     this.isHovered = true;
+    this.isLoading = true;
 
-    const response: any = await this.gitApiService.getRepositoryReadme(item.full_name);
-    this.readme = response.content ? atob(response.content) : null;
+    try{
+      const response: any = await this.gitApiService.getRepositoryReadme(item.full_name);
+      this.readme = response.content ? atob(response.content) : null;
+    }catch(error){
+      console.error('Error fetching README:', error);
+      this.readme = null;
+    }
+
+    this.isLoading = false;
+    
   }
 
   onMouseMove(event: MouseEvent){
@@ -58,7 +71,8 @@ export class DashboardComponent {
 
     const viewportWidth = window.innerWidth;
 
-    this.hoverDirection = this.mouseX + 550 > viewportWidth ? 'left' : 'right';
+    this.containerWidth  = this.hoverContainer?.nativeElement.clientWidth || 300;
+    this.hoverDirection = this.mouseX + this.containerWidth > viewportWidth ? 'left' : 'right';
   }
 
   onMouseLeave(){
